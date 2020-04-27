bbf013c160df234540f946177d0206f4ed360162
kostyash23 <kostiashmootoloha@gmail.com>
2020-04-27 11:31:58 +0300

    com-2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(function () {
   $(".slider").slick({
     nextArrow:
-      '<button class="slick-arrow slick-next"><img src="../images/slick-next.svg" alt="arrow" /></button>',
+      '<button class="slick-arrow slick-next"><img src="./images/slick-next.svg" alt="arrow" /></button>',
     prevArrow:
-      '<button class="slick-arrow slick-prev"><img src="../images/slick-prev.svg" alt="arrow" /></button>',
+      '<button class="slick-arrow slick-prev"><img src="./images/slick-prev.svg" alt="arrow" /></button>',
     responsive: [
       {
         breakpoint: 441,
