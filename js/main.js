0c1101be8ce92e40c12b522b3cd835eecb08f11a
kostyash23 <kostiashmootoloha@gmail.com>
2020-04-27 11:20:24 +0300

    final

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+  $(".slider").slick({
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="../images/slick-next.svg" alt="arrow" /></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="../images/slick-prev.svg" alt="arrow" /></button>',
+    responsive: [
+      {
+        breakpoint: 441,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
